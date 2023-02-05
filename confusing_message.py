602cac3e43ddbfc56f0f8fec71dd508d06e7e474
twentyWenSavage <120768906+twentyWenSavage@users.noreply.github.com>
2023-02-04 23:06:01 -0600

    Update confusing_message.py
    Indentation problems

--- confusing_message.py
+++ confusing_message.py
@@ -2,8 +2,7 @@ import random
 
 def unknown():
     response = ["Could you please re-phrase that? ", 
-                "Sorry I can’t really understand.",
+                "Sorry I can't really understand.",
                 "Please call the customer service number at XXX-XXX-XXXX.",
-                "What does that mean?"]
-        [random.randrange(4)]
+                "What does that mean?"][random.randrange(4)]
     return response

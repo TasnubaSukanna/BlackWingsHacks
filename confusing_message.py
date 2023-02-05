dc3741bff7d4f0911ded40addc37427b7abde149
TasnubaSukanna <88466390+TasnubaSukanna@users.noreply.github.com>
2023-02-05 01:04:22 -0500

    Update confusing_message.py

--- confusing_message.py
+++ confusing_message.py
@@ -1,8 +1,5 @@
 import random
 
 def unknown():
-    response = ["Could you please re-phrase that? ", 
-                "Sorry I can't really understand.",
-                "Please call the customer service number at XXX-XXX-XXXX.",
-                "What does that mean?"][random.randrange(4)]
+    response = ["Could you please re-phrase that? ", "Sorry I can't really understand.", "Please call the customer service number at XXX-XXX-XXXX.","What does that mean?"][random.randrange(4)]
     return response

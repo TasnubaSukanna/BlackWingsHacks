46913e7da024f162abb9168f5378a03881add04c
TasnubaSukanna <88466390+TasnubaSukanna@users.noreply.github.com>
2023-02-04 21:45:56 -0500

    Update and rename long_responses.py to confusing_message.py

---
+++ confusing_message.py
@@ -0,0 +1,9 @@
+import random
+
+def unknown():
+    response = ["Could you please re-phrase that? ", 
+                "Sorry I can’t really understand.",
+                "Please call the customer service number at XXX-XXX-XXXX.",
+                "What does that mean?"]
+        [random.randrange(4)]
+    return response
